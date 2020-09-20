0266c4ca263b4c10568d214bc1d8c1f6c9d3034d
”ritsuki <usefull3567tuwa@gmail.com>
2020-09-20 07:48:10 +0000

    php02kadaia

---
+++ kadai2.php
@@ -0,0 +1,35 @@
+<?php
+//課題１
+$name = "大坪";
+if ($name == "田中") {
+   echo "あなたの名前ではありません";
+ } else {
+   echo "私は大坪です" . "\n";
+ }
+ 
+//課題２
+$total = 0;
+
+for ($i = 0; $i <= 1000; $i++) {
+  $total += $i;
+}
+echo $total. "\n";
+
+//課題３
+$fruits = array("りんご", "ぶどう", "みかん", "レモン", "ばなな");
+foreach($fruits as $fruits)
+  echo  $fruits;
+  echo "\n";
+  
+//応用課題
+$start = 1;
+$end = 100;
+
+for($i = $start; $i < $end; $i++){
+
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0){
+    echo $i;
+    echo "\n";
+  }
+}
